034208551f45697c573bd6c6905b018a21e8f8ce
Danilo Salvio <danilo_salvio@live.com>
2023-04-01 20:46:36 -0300

    Corrects cf configurations.

--- cf.tf
+++ cf.tf
@@ -6,6 +6,13 @@ resource "aws_cloudfront_distribution" "root_cloud_front" {
   enabled      = true
   http_version = "http2"
 
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
   origin {
     domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name # THIS COULD BE WRONG
     origin_id   = local.cfn
@@ -13,6 +20,7 @@ resource "aws_cloudfront_distribution" "root_cloud_front" {
       http_port              = 80
       https_port             = 443
       origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.3"] #1.2?
     }
   }
 
@@ -22,7 +30,12 @@ resource "aws_cloudfront_distribution" "root_cloud_front" {
     viewer_protocol_policy = "redirect-to-https"
     forwarded_values {
       query_string = false
+      cookies {
+        forward = "none" # could be wrong
+      }
     }
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"] # could be wrongs
   }
 
   custom_error_response {
@@ -34,8 +47,8 @@ resource "aws_cloudfront_distribution" "root_cloud_front" {
 
   aliases = [var.domain_name]
 
-#   viewer_certificate {
-#     acm_certificate_arn = "" # CERTF ARN HERE
-#     ssl_support_method  = "sni-only"
-#   }
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
+  }
 }
