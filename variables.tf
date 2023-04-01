8b2770e1a043f760a729145da893a469e847dce3
Danilo Salvio <danilo_salvio@live.com>
2023-04-01 20:54:06 -0300

    Variable validation.

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "domain_name" {
-  type = string
+  type        = string
+  description = "The naked domain name without www."
+
+  validation {
+    condition     = !(substr(var.domain_name, 0, 4) == "www.")
+    error_message = "You must provide the naked domain name without www so that we can redirect one to the other."
+  }
 }
