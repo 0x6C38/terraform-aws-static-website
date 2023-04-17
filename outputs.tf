d1c7233744194d0fa382d6a10842d31a93d764f0
Danilo Salvio <danilo_salvio@live.com>
2023-04-17 16:41:36 -0300

    More outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "s3_redirect_endpoint" {
+  value       = aws_s3_bucket_website_configuration.redirect_bucket_website_config.website_endpoint
+  description = "S3 redirect endpoint."
+}
+
+output "s3_website_endpoint" {
+  value       = aws_s3_bucket_website_configuration.bucket_website_config.website_endpoint
+  description = "S3 website endpoint."
+}
+
 output "cf_domain_name" {
   value       = aws_cloudfront_distribution.root_cloud_front.domain_name
   description = "Cloud front root distribution domain name."
@@ -6,6 +16,11 @@ output "cf_domain_name" {
 output "rcf_domain_name" {
   value       = aws_cloudfront_distribution.redirect_cloud_front.domain_name
   description = "Redirect cloud front root distribution domain name."
+}
+
+output "domain_name" {
+  value       = local.domain_name
+  description = "The domain name where the website is hosted."
 }
 
 output "upload_command" {
